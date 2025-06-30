1f2b426309776ad7ecec786efd04410809f1f7d2
Yash shiva <124360143+Yashshiva75@users.noreply.github.com>
2025-06-30 23:06:55 +0530

    send messge

--- backend/controllers/MessageController.js
+++ backend/controllers/MessageController.js
@@ -49,6 +49,7 @@ export const getMessages = async(req,res)=>{
       return res.status(200).json('Successfully got message')
 
    }catch(error){
+      console.log('Err in convo',error)
       return res.status(500).json('error in get mesg')
    }
 }
