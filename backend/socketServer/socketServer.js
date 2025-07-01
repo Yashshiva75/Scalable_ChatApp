0e9d31b6f43ed84bc371d1c7d5c0aa9e6f2ea9fa
Yash shiva <124360143+Yashshiva75@users.noreply.github.com>
2025-07-01 22:53:36 +0530

    socket server added

---
+++ backend/socketServer/socketServer.js
@@ -0,0 +1,36 @@
+import { Server } from 'socket.io'
+
+
+let io;
+
+export const initSocket = (server) => {
+  io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      credentials: true
+    }
+  });
+
+  io.on('connection', (socket) => {
+    console.log('✅ New user connected:', socket.id);
+
+    socket.on('sendMessage', (data) => {
+      console.log('📩 Message Received:', data);
+      io.emit('receiveMessage', data);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('❌ User disconnected:', socket.id);
+    });
+  });
+
+  return io;
+};
+
+
+export const getIO = () => {
+  if (!io) {
+    throw new Error("Socket.io not initialized");
+  }
+  return io;
+};
