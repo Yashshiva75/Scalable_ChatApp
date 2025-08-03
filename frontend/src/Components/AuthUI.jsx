dcbc9def9e7654cd12875f7bdfd7e77e90992c9c
Yash shiva <124360143+Yashshiva75@users.noreply.github.com>
2025-08-03 11:31:20 +0530

    Update AuthUI.jsx

--- frontend/src/Components/AuthUI.jsx
+++ frontend/src/Components/AuthUI.jsx
@@ -344,17 +344,19 @@ export default function AuthUI() {
             or continue with
           </div>
 
-          <div className="">
-            <GoogleLogin
-              onSuccess={handleGoogleLoginSuccess}
-              onError={() => showErrorToast("Google Login Failed")}
-              theme="outline" 
-              size="large"
-              shape="pill"
-              text="continue_with"
-              width="100%"
-            />
+          <div className="w-full flex justify-center mt-4">
+            <div className="w-full">
+              <GoogleLogin
+                onSuccess={handleGoogleLoginSuccess}
+                onError={() => showErrorToast("Google Login Failed")}
+                theme="outline"
+                size="large"
+                shape="pill"
+                text="continue_with"
+              />
+            </div>
           </div>
+
 
           {/* Footer Text */}
           <p className="text-center text-sm text-slate-500 mt-6">
